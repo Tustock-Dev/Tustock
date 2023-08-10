798d03231bc503555b5288a0f115e73c6ee910e7
braxvalle <braxvalle2@gmail.com>
2023-08-10 20:46:54 +0300

    changes

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const bar = document.querySelector('.bars');
 const nav = document.querySelector('.ul-screen');
+const navLinks = document.querySelectorAll('nav li')
+const activepage = window.location.pathname;
 
 bar.addEventListener('click', function(){
    bar.classList.toggle('active');
    nav.classList.toggle('active');
 })
+
+
+navLinks.forEach(li => {
+   li.addEventListener('click', () => {
+      li.classList.add('li')
+   })
+});
