20af9efb7565bbff347a96b35e8b2e4764d54804
braxvalle <braxvalle2@gmail.com>
2023-08-14 21:07:42 +0300

    changes

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const bar = document.querySelector('.bars');
-const nav = document.querySelector('.ul-screen');
-const navLinks = document.querySelectorAll('nav ul a');
+const bar = document.querySelector(".bars");
+const nav = document.querySelector(".ul-screen");
+const navLinks = document.querySelectorAll("nav ul a");
 const activepage = window.location.pathname;
 
-bar.addEventListener('click', function(){
-   bar.classList.toggle('active');
-   nav.classList.toggle('active');
-})
+bar.addEventListener("click", function () {
+  nav.classList.toggle("active");
+  /* bar.classList.toggle("active"); */
+});
 
-navLinks.forEach(links => {
-   const navlinksPath = new URL(links.href).pathname;
+navLinks.forEach((links) => {
+  const navlinksPath = new URL(links.href).pathname;
 
-   if(activepage === navlinksPath) {
-      links.classList.add('current'); 
-   }
+  if (activepage === navlinksPath) {
+    links.classList.add("current");
+  }
 });
