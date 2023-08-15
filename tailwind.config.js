f5e8e565eda3189a3358c0769872756a6936d7a9
Mark <muchirimark2@gmail.com>
2023-08-15 18:33:35 +0300

    added tailwind files

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./coparteners.html", "./team.html", "./profile.html", "./investors.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
